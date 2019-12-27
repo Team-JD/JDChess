e8e722af5cc6fdcad60590785ba99a3a279a6a3b
Dseventeen <53230645+Dseventeen@users.noreply.github.com>
2019-12-26 17:01:16 -0800

    Create games_controller.rb

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,18 @@
+class GamesController < ApplicationController
+  def new
+    @game = Game.new
+  end
+
+  def create
+    @pgame = current_user.game.create(game_params)
+    if @game.valid?
+        redirect_to root_path
+  end
+
+  def show
+    @game = Game.find(params[:id])
+  end
+
+  def index
+  end
+end
