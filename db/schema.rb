86adfb635ee20df58ba6b5ebc9a36edbe257c9c7
Dseventeen <53230645+Dseventeen@users.noreply.github.com>
2019-12-26 17:03:04 -0800

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_12_010356) do
+ActiveRecord::Schema.define(version: 2019_12_27_000347) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "games", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
