3fc84bf23b88d1140f89dba7d5b5008a59ff508a
Dan <dseventeen@gmail.com>
2020-01-03 01:58:13 +0000

    hard-coded pieces into the board

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,2 +1,50 @@
-class Game < ApplicationRecord
+class Game < ApplicationRecord::Base
+  after_create :initialize_board
+
+  def initialize_board
+    
+    (0..7).each do |i|
+      Pawn.create(
+        game_id: id,
+        x_position: i,
+        y_position: 1,
+        color: false
+        )
+    end
+
+    Rook.create(game_id: id, x_position: 0, y_position: 0, color: true)
+    Rook.create(game_id: id, x_position: 7, y_position: 0, color: true)
+
+    Knight.create(game_id: id, x_position: 1, y_position: 0, color: true)
+    Knight.create(game_id: id, x_position: 6, y_position: 0, color: true)
+
+    Bishop.create(game_id: id, x_position: 2, y_position: 0, color: true)
+    Bishop.create(game_id: id, x_position: 5, y_position: 0, color: true)
+
+    Queen.create(game_id: id, x_position: 3, y_position: 0, color: true)
+    King.create(game_id: id, x_position: 4, y_position: 0, color: true)
+
+    # Black Pieces
+    (0..7).each do |i|
+      Pawn.create(
+        game_id: id,
+        x_position: i,
+        y_position: 6,
+        color: true
+        )
+    end
+
+    Rook.create(game_id: id, x_position: 0, y_position: 7, color: false)
+    Rook.create(game_id: id, x_position: 7, y_position: 7, color: false)
+
+    Knight.create(game_id: id, x_position: 1, y_position: 7, color: false)
+    Knight.create(game_id: id, x_position: 6, y_position: 7, color: false)
+
+    Bishop.create(game_id: id, x_position: 2, y_position: 7, color: false)
+    Bishop.create(game_id: id, x_position: 5, y_position: 7, color: false)
+
+    Queen.create(game_id: id, x_position: 3, y_position: 7, color: false)
+    King.create(game_id: id, x_position: 4, y_position: 7, color: false)
+  end
+end
 end
