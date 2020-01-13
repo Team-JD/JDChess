da0c20f52a9860e3b0d155f9e9dde1f30d587e0d
joel pankito <joelpankito@yahoo.com>
2020-01-13 02:16:17 +0000

    added helper model point

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,4 +1,5 @@
 class GamesController < ApplicationController
+
   def new
     @game = Game.new
   end
@@ -10,15 +11,14 @@ class GamesController < ApplicationController
 
   def show
     @game = Game.find(params[:id])
+    @pieces = @game.pieces
   end
 
   def index
   end
-
   private
 
   def game_params
     params.require(:game).permit(:player1, :player2, :status, :winner, :loser, :current_turn, :player1Color, :player2Color, :started, :ended)
   end
-
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_26_022451) do
+ActiveRecord::Schema.define(version: 2020_01_05_075338) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 2019_12_26_022451) do
     t.datetime "updated_at", null: false
   end
 
+
   create_table "pieces", force: :cascade do |t|
     t.integer "game_id"
     t.string "player"
@@ -41,6 +42,7 @@ ActiveRecord::Schema.define(version: 2019_12_26_022451) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
