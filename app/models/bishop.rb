30b9f944d8d437d90ae7027139eb7215db65de87
joel pankito <joelpankito@yahoo.com>
2020-01-13 02:24:29 +0000

    added valid moves

--- app/models/bishop.rb
+++ app/models/bishop.rb
@@ -1,2 +1,12 @@
 class Bishop < Piece  
+    def valid_move?(curr_loc,dest_loc)
+        current_point = Point.from_location(curr_loc)
+        destination_point = Point.from_location(dest_loc)
+
+        if (current_point.y - destination_point.y).abs == (current_point.x - destination_point.x).abs
+            return true
+        else
+            return false
+        end
+    end 
 end
