360588475830edaaaded760f47f5630b96c28476
joel pankito <joelpankito@yahoo.com>
2020-01-13 02:35:23 +0000

    fixed the issue

--- spec/models/bishop_spec.rb
+++ spec/models/bishop_spec.rb
@@ -2,11 +2,14 @@ require 'rails_helper'
 
 RSpec.describe Bishop, type: :model do
     describe "#is_obstructed" do
-        it "should return true if move is valid and no piece in it path" do
-            game = game.create
-            blk_bishop1 = FactoryBot.create(:bishop, location: "A3", game_id: game.id, player: user1.email, color: "B", type: "bishop")
-            blk_Pawn1 = FactoryBot.create(:pawn, location: "D4", game_id: game.id, player: user1.email, color: "B", type: "Pawn")
-            result = blk_bishop1.valid_move?(blk_bishop1.location,"C5")
+        it "should return true if move is valid " do
+            bishop = Bishop.new
+            bishop.location = "A1"
+            allow(Piece).to receive(:where).and_call_original
+            result = bishop.valid_move?(bishop.location, "H8")
+            expect(result).to eq (true)
+            bishop.location = "A8"
+            result = bishop.valid_move?(bishop.location, "H1")
             expect(result).to eq (true)
         end
     end
--- spec/models/king_spec.rb
+++ spec/models/king_spec.rb
@@ -2,12 +2,25 @@ require 'rails_helper'
 
 RSpec.describe King, type: :model do
     describe "#is_obstructed" do
-        it "should return true if move is valid and no piece in it path" do
-            user1 = FactoryBot.create(:user)
-            game = user1.games.create
-            blk_king1 = FactoryBot.create(:king, location: "D1", game_id: game.id, player: user1.email, color: "B", type: "king")
-            blk_Pawn1 = FactoryBot.create(:pawn, location: "D2", game_id: game.id, player: user1.email, color: "B", type: "Pawn")
-            result = blk_king1.valid_move?(blk_king1.location,"E1")
+        it "should return true if move is valid " do
+            king = King.new
+            king.location = "C3"
+            allow(Piece).to receive(:where).and_call_original
+            result = king.valid_move?(king.location, "C2")
+            expect(result).to eq (true)
+            result = king.valid_move?(king.location, "C4")
+            expect(result).to eq (true)
+            result = king.valid_move?(king.location, "B4")
+            expect(result).to eq (true)
+            result = king.valid_move?(king.location, "B3")
+            expect(result).to eq (true)
+            result = king.valid_move?(king.location, "B2")
+            expect(result).to eq (true)
+            result = king.valid_move?(king.location, "D4")
+            expect(result).to eq (true)
+            result = king.valid_move?(king.location, "D3")
+            expect(result).to eq (true)
+            result = king.valid_move?(king.location, "D2")
             expect(result).to eq (true)
         end
     end
